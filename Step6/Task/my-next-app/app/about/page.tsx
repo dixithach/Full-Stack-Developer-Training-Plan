42e2f126e65ca8e73a61d34d3d245fab75c6c7d9
dixiach <dixiach@gmail.com>
2025-11-05 14:26:51 +0530

    Added about folder inside it page.jsx file is added

---
+++ Step6/Task/my-next-app/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <section>
+      <h1>About</h1>
+      <p>This content is rendered at /about using the App Router.</p>
+    </section>
+  );
+}
