1a93d8017cf964ea745d800c6ab11bee9aa47279
dixiach <dixiach@gmail.com>
2025-11-06 05:53:05 +0530

    Created the /contact page with contact as folder and inside it page.tsx app/contact/page.tsx

---
+++ Step6/Exercise/my-next-app/app/contact/page.tsx
@@ -0,0 +1,11 @@
+// app/contact/page.tsx
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <section>
+      <h1>Contact</h1>
+      <p>Put contact info or a contact form here.</p>
+    </section>
+  );
+}
