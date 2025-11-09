0de814fe45fa45dfbed42aef104910f909db1c29
dixiach <dixiach@gmail.com>
2025-11-10 04:46:20 +0530

    Define a UniversityCourse interface is done in patientuni.test.ts file

--- Step8/Exercise/patientuni.test.ts
+++ Step8/Exercise/patientuni.test.ts
@@ -6,3 +6,12 @@ interface Patient {
   age: number;
   condition: string;
 }
+
+
+// Define a UniversityCourse interface
+
+ interface UniversityCourse {
+  title: string;
+  code: string;
+  instructor: string;
+}
