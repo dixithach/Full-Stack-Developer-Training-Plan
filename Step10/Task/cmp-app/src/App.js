7ee3b3cc616d82f59cbb975e5fa65277a1ad23f5
dixiach <dixiach@gmail.com>
2025-11-14 06:05:28 +0530

    Added COUNTER STATE in src/App.js file

--- Step10/Task/cmp-app/src/App.js
+++ Step10/Task/cmp-app/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  // ---------------------------
+  // 1) COUNTER STATE
+  // ---------------------------
+
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+
+
+return (
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+
+      {/* --------------------------- */}
+      {/*       COUNTER SECTION       */}
+      {/* --------------------------- */}
+      <h1>React Practice: Counter + Input + Todo</h1>
+
+      <section style={{ marginBottom: 40 }}>
+        <h2>Counter</h2>
+        <p>Current Count: <strong>{count}</strong></p>
+
+        <button onClick={decrement}>-</button>
+        <button onClick={increment} style={{ marginLeft: 8 }}>+</button>
+        <button onClick={reset} style={{ marginLeft: 8 }}>Reset</button>
+      </section>
+     </div>
+      );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-export default App;
