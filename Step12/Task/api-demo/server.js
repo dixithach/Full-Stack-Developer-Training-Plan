0affffe74b4258b5db23edc3992a34c4e252602c
dixiach <dixiach@gmail.com>
2025-11-19 21:26:30 +0530

    Added server.js file inside api-demo and coded inside it for post and get route

---
+++ Step12/Task/api-demo/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// GET route
+app.get("/welcome", (req, res) => {
+  res.send("Hello! This is a GET request.");
+});
+
+// POST route
+app.post("/send-data", (req, res) => {
+  const dataFromClient = req.body;
+
+  res.send({
+    message: "POST request received!",
+    yourData: dataFromClient
+  });
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
