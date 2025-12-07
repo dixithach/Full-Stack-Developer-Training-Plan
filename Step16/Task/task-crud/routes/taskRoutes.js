96c7c8219b1c0bd37c6a76ece02972e59534cba8
dixiach <dixiach@gmail.com>
2025-12-07 06:05:30 +0530

    Next i created routes folder inside it i created a file by name taskRoutes.js and started coding it with Hook controller functions to Express routes

---
+++ Step16/Task/task-crud/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+// routes/taskRoutes.js
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+router.post('/', taskController.createTask);
+router.get('/', taskController.getAllTasks);
+router.get('/:id', taskController.getTaskById);
+router.put('/:id', taskController.updateTask);
+router.patch('/:id', taskController.patchTask);
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router;
