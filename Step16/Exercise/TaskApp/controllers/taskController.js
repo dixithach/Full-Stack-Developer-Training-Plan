f4b70b4c57ba9f8210a81b5d67dba613c49f765c
dixiach <dixiach@gmail.com>
2025-12-08 06:11:27 +0530

    Created controllers folder inside Taskapp and than inside controllers created a file by name taskController.js and started coding it with CREATE TASK,GET ALL TASKS,UPDATE TASK,DELETE TASK

---
+++ Step16/Exercise/TaskApp/controllers/taskController.js
@@ -0,0 +1,53 @@
+const Task = require('../models/Task');
+
+// CREATE TASK
+const createTask = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const saved = await task.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// GET ALL TASKS
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// UPDATE TASK
+const updateTask = async (req, res) => {
+  try {
+    const updated = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// DELETE TASK
+const deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  createTask,
+  getAllTasks,
+  updateTask,
+  deleteTask
+};
