e1040926fac74ef17026a00defb46b83384f43a3
dixiach <dixiach@gmail.com>
2026-02-09 21:37:30 +0530

    Added script.js file inside task-manager folder which is used to add task and delete a task it makes interactive and give result when a button is clicked

---
+++ Step22/Task/task-manager/script.js
@@ -0,0 +1,27 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value;
+
+  if (taskText === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+});
